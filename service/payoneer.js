2dc1f9c5c8d9fa23a4711b7fb9c38507a8b8799e
Deepan Suthakar <udhay271192@gmail.com>
2023-05-22 22:21:10 -0400

    Update payoneer.js

--- service/payoneer.js
+++ service/payoneer.js
@@ -11,12 +11,35 @@ class PayoutService {
   }
 
   /**
+   * This method is used to determine required banking
+   * fields for a given country, currency and payee type
+   * @param {*} countryCode
+   * @param {*} currencyCode
+   * @param {*} payeeType
+   * @returns
+   */
+
+  async bankingInfo (countryCode, currencyCode, payeeType) {
+    const url = `${config.payoneerApi}/${config.programId}/payout-methods/bank` +
+                `/account-types/${payeeType}/countries/${countryCode}/currencies/${currencyCode}`
+    const authToken = await auth.getToken()
+    const response = await sendRequest(url, {
+      method: 'GET',
+      headers: {
+        ...this.headers,
+        Authorization: `Bearer ${authToken}`
+      }
+    })
+    return response
+  }
+
+  /**
    * This method is used to check the status of a payout
    * @param {*} clientRefId
    * @returns PAYOUT STATUS
    */
   async payoutStatus (clientRefId) {
-    const url = `${config.payoneerApi}/v4/programs/${config.programId}/payouts/${clientRefId}/status`
+    const url = `${config.payoneerApi}/${config.programId}/payouts/${clientRefId}/status`
     const authToken = await auth.getToken()
     console.log(`[payoutStatus] authToken: ${authToken}`)
     const response = await sendRequest(url, {
@@ -35,7 +58,7 @@ class PayoutService {
    * @returns mass payout response
    */
   async initiatePayout (req) {
-    const url = `${config.payoneerApi}/v4/programs/${config.programId}/masspayouts`
+    const url = `${config.payoneerApi}/${config.programId}/masspayouts`
     const authToken = await auth.getToken()
     const payload = this.framePayoutRequest(req)
     const response = await sendRequest(url, {
